1ef6dd11ee450b3871ad5f27de6f9c026e471d89
rakeshcooper <rakeshmurali097@gmail.com>
2022-10-20 00:48:11 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,22 @@
+let double = 1;
+while(double <= 10){
+    console.log(double);
+    double += 1;
+}
+
+let doubles = 1;
+
+
+do{
+    console.log(doubles);
+    doubles++;
+}while(doubles <= 10)
+
+
+
+for(let double=1; double <= 10; double+=1){
+    console.log(double);
+
+}
+
+
